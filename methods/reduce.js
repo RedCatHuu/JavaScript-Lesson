325f9a06fb4b2bf9ddaa2e0ecbdc68cdf08ce835
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-03-02 08:54:24 +0000

    [add]配列内要素で最も多いものを表示

--- methods/reduce.js
+++ methods/reduce.js
@@ -27,3 +27,31 @@ const average = numbers.reduce((acc, val, index, ary) => {
   }
 }, 0); // 初期値は0
 console.log(average); // 6
+
+// 出現回数が最も多いものを表示。
+const fruits = ["grape", "banana", "banana", "apple", "banana", "peach", "banana", "grape", "apple", "apple"]
+
+const fruitsCounts = {}; // 連想配列
+for (const item of fruits) {
+    fruitsCounts[item] = (fruitsCounts[item] || 0) + 1;
+}
+// 累積値と現在の値を比べ、大きいほうを次の累積値とする
+const most = Object.keys(fruitsCounts).reduce((acc, val) => fruitsCounts[acc] > fruitsCounts[val] ? acc : val);
+console.log(most); // banana
+console.log(fruitsCounts); // { grape: 2, banana: 4, apple: 3, peach: 1 }
+
+// 最初はこう記述した。
+let name = fruits
+let ary = {};
+for(let item of name){
+    if( Object.keys(ary).includes(item) ){
+        ary[item] += 1
+    }else{
+        ary[item] = 0
+    }
+}
+let maxFruits = Object.values(ary).reduce( (acc, val) =>{
+    return Math.max(acc, val);
+}, 0)
+const key = Object.keys(ary).find((key) => ary[key] == maxFruits)
+console.log(key)
